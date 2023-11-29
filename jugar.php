eab5b9f031eb56f090d8055ed8097fc590143a33
IvanBelio <ivanbeliogil0@gmail.com>
2023-11-29 14:11:54 +0100

    Ejercicio Terminado

---
+++ jugar.php
@@ -0,0 +1,110 @@
+<?php
+//Declaro variables
+//$min=0;
+//$max=0;
+//$numero_propuesto=0;
+//$jugada=0;
+//$intentos=0;
+//$resultado=0;
+
+
+
+$opcion = $_POST['submit'] ?? null;
+switch ($opcion){
+    case "Reiniciar":
+    case "Empezar":
+        //Esto es el Input
+        //Rango menor
+        $min=0;
+        //Número de intentos que le pasamos
+        $intentos=$_POST['intentos'];
+        $max= 2** $intentos;
+        //Esto es el procesamiento
+        $numero_propuesto= ($min+$max)/2;
+        //Número de la jugada
+        $jugada= 1;
+        break;
+
+    case "Jugar":
+        //Obtener los valores de variables
+        $min=$_POST['min'];
+        $max=$_POST['max'];
+        //Número de intentos que le pasamos
+        $intentos=$_POST['intentos'];
+        //Número de la jugada
+        $jugada= $_POST['jugada'];
+        //Número que ha salido en el caso de empezar y, este parámetro lo tenemos guardado en un input tipo hidden
+        $numero_propuesto = $_POST['numero_propuesto'];
+
+        //Leer resultado
+        $resultado = $_POST['resultado'];
+
+        //Actualizar mínimo o máximo en función del resultado
+        switch ($resultado){
+            case "mayor":
+                $min = $numero_propuesto;
+                break;
+            case "menor":
+                $max = $numero_propuesto;
+                break;
+            case "igual":
+                // TO DO falta implementar esta situación que será fin de juego
+                //Enviamos la variable con texto
+                header('Location:fin.php?msj=Has acertado');
+                exit();
+        }
+
+        //PROCEDIMIENTO_____Actualizar las variables $numero_propuesta $jugada
+        $jugada++;
+        if ($jugada>$intentos){
+            header('Location:fin.php?msj=Te has quedado sin intentos');
+            exit();
+        }
+
+        $numero_propuesto = ($min+$max)/2;
+        break;
+
+    case "Volver";
+    default:
+        header('Location:index.php');
+        exit();
+
+
+}
+?>
+
+<!doctype html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Juego de adivina un número</title>
+</head>
+<body>
+<fieldset style="width:40%;background:bisque ">
+    <legend>Empieza el juego</legend>
+    <form action="jugar.php" method="POST" >
+        <h2> El n&uacutemero es  <span style="color: blue"> <?=$numero_propuesto?></span> </h2>
+        <h5> Jugada  <span style="color: blue"><?=$jugada?></span>  </h5>
+        <h5> Actualmente te quedan   <span style="color: blue"> <?=$intentos-$jugada?></span> intentos </h5>
+
+        <input type="hidden" value="10" name="intentos">
+        <fieldset>
+            <legend>Indica c&oacutemo es el n&uacutemero qu&eacute has pensado</legend>
+            <input type="radio" name="rtdo" checked value='>'> Mayor<br />
+            <input type="radio" name="rtdo" value='<'> Menor<br />
+            <input type="radio" name="rtdo" value='='> Igual<br />
+        </fieldset>
+        <hr />
+        <input type="submit" value="Jugar" name="submit" >
+        <input type="submit" value="Reiniciar" name="submit"  >
+        <input type="submit" value="Volver" name="submit"  >
+        <input type="hidden" name="max" value="<?=$max?>">
+        <input type="hidden" name="min" value="<?=$min?>">
+        <input type="hidden" name="numero_propuesto" value="<?=$numero_propuesto?>">
+        <input type="hidden" name="intentos" value="<?=$intentos?>">
+        <input type="hidden" name="jugada" value="<?=$jugada?>">
+    </form>
+</fieldset>
+
+</body>
+</html>
---
+++ readme.md
@@ -0,0 +1,11 @@
+# Adivinar un número
+Este es un proyecto de PHP que adivina un número
+
+El usuario piensa un número de un intervalo previamente establecido.
+
+Para clonártelo escribe en el terminal
+```shell
+git clone https://github.com/IvanBelio/adivinaNumero.git
+```
+
+He cambiado a SSH
